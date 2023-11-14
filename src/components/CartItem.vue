8fa3845dde3558397fb6c6dd4cfc1883f2a0ab89
Aman <beyond.http@gmail.com>
2023-11-14 21:32:44 +0600

    cart items git status "

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="cart-item d-flex">
-    <span class="me-auto">
-      <span class="font-weight-bold"> 1. Кабель Borofone ZX 400, </span>
-      1 шт
-    </span>
-    <span> 1000 </span>
-  </div>
+  <v-menu location="bottom">
+    <template v-slot:activator="{ props }">
+      <div v-bind="props" class="cart-item d-flex">
+        <span class="me-auto">
+          <span class="font-weight-bold"> {{ item.title }}, </span>
+          {{ item.count }} шт.
+        </span>
+        <span> {{ item.price }}</span>
+      </div>
+    </template>
+    <v-list class="border elevation-0">
+      <v-list-item
+        v-for="(item, i) in menuItems"
+        :prepend-icon="item.icon"
+        @click=""
+        :key="i"
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
+
+<script setup>
+import { ref } from "vue"
+
+defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+})
+
+const menuItems = ref([
+  {
+    icon: "mdi-arrow-right",
+    title: "Перейти к товару",
+  },
+  {
+    icon: "mdi-close",
+    title: "Удалить",
+  },
+])
+</script>
 
 <style scoped>
 .cart-item :hover {
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="8">
         <v-text-field placeholder="Код товара"></v-text-field>
-        <cart-item></cart-item>
+        <cart-item v-for="(item, i) in items" :item="item" @click="" :key="i" />
       </v-col>
       <v-col>
         <v-select></v-select>
@@ -17,6 +17,23 @@
 <script setup>
 import CartItem from "@/components/CartItem.vue"
 import CartTotalValue from "@/components/CartTotalValue.vue"
+
+import { ref } from "vue"
+
+const items = ref([
+  {
+    code: 123124124,
+    title: "Кабель Borofone ZX 2000",
+    count: 2,
+    price: 2000,
+  },
+  {
+    code: 325436346,
+    title: "ЗУ Hoco TX 23",
+    count: 2,
+    price: 1500,
+  },
+])
 </script>
 
 <style scoped>
