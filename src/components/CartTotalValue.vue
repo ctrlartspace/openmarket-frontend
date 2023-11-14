05b47e19440006ab7649254aabbd89636ff42cd9
Aman <beyond.http@gmail.com>
2023-11-14 20:47:43 +0600

    add icons

--- src/components/CartTotalValue.vue
+++ src/components/CartTotalValue.vue
@@ -1,7 +1,11 @@
 <template>
   <v-card variant="elevated" color="success">
     <v-card-text class="d-flex text-white">
-      <v-icon icon="mdi-domain"></v-icon>
+      <v-icon
+        class="mr-4 align-self-center"
+        size="large"
+        icon="mdi-cart-outline"
+      ></v-icon>
       <span class="text-h5 font-weight-bold me-auto"> 1000 </span>
       <span class="text-h5 font-weight-bold"> KZT </span>
     </v-card-text>
