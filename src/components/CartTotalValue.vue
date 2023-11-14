c3b617f15cbbc07b0a6f0d04f3d3a04d74a65648
Aman <beyond.http@gmail.com>
2023-11-14 21:42:51 +0600

    card total button border

--- src/components/CartTotalValue.vue
+++ src/components/CartTotalValue.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-card variant="elevated" color="success">
+  <v-card class="border" variant="elevated" color="success">
     <v-card-text class="d-flex text-white">
       <v-icon
         class="mr-4 align-self-center"
         size="large"
         icon="mdi-cart-outline"
       ></v-icon>
-      <span class="text-h5 font-weight-bold me-auto"> 1000 </span>
+      <span class="text-h5 font-weight-bold me-auto"> 1 000 </span>
       <span class="text-h5 font-weight-bold"> KZT </span>
     </v-card-text>
   </v-card>
