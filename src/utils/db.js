568df1b228dda062e1d06caaffbba89382f2cc64
Aman <beyond.http@gmail.com>
2023-11-15 09:19:28 +0600

    add db js

---
+++ src/utils/db.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    id: 1,
+    code: 123123123,
+    model: "ZX 123",
+    brand: "Borofone",
+    type: "USB",
+    price: 2000,
+  },
+  {
+    id: 2,
+    code: 234234234,
+    model: "GT 34",
+    brand: "Hoco",
+    type: "USB",
+    price: 1000,
+  },
+]
