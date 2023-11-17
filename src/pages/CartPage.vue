3b8d82badd71d9fb931a9f3f1d986dcef222b468
Aman <beyond.http@gmail.com>
2023-11-17 16:53:17 +0600

    cart list

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,22 +1,43 @@
 <template>
-  <div class="grid grid-cols-6 gap-4 h-full max-h-96 mt-32 overflow-hidden">
+  <div class="grid grid-cols-6 gap-4 mt-32">
     <div class="col-span-4">
-      <search-field
-        class="mb-4"
-        v-model="inputValue"
-        :search-items="searchItems"
-        @submit="addCartItem"
-        @change="search"
-        @on-search-item-click="addCartItem"
-        autoclear
-      />
-      <cart-item
-        v-for="(item, i) in store.cartItems"
-        :item="item"
-        @click=""
-        @on-remove-click="store.removeItem(i)"
-        :key="i"
-      />
+      <div class="h-96 overflow-hidden">
+        <search-field
+          class="mb-4"
+          v-model="inputValue"
+          :search-items="searchItems"
+          @submit="addCartItem"
+          @change="search"
+          @on-search-item-click="addCartItem"
+          autoclear
+        />
+        <cart-item
+          v-for="(item, i) in store.cartItems"
+          :item="item"
+          @click=""
+          @on-remove-click="store.removeItem(i)"
+          :key="i"
+        />
+      </div>
+      <hr class="h-px bg-gray-200 border-0" />
+      <div>
+        <div
+          v-for="i in 1"
+          class="inline-block text-lg text-gray-300 px-4 py-2 cursor-pointer border-b border-r hover:bg-gray-100 first:rounded-br-none first:border-l first:rounded-bl last:rounded-br first:text-black"
+        >
+          <div class="flex">
+            <span class="self-center material-icons text-lg">
+              shopping_cart</span
+            >
+            <span class="ml-2"> {{ i }} </span>
+          </div>
+        </div>
+        <div
+          class="inline-block text-lg text-gray-300 px-4 py-2 cursor-pointer border-b border-r hover:bg-gray-100 first:rounded-br-none first:border-l first:rounded-bl last:rounded-br"
+        >
+          <span class="text-lg self-center">+</span>
+        </div>
+      </div>
     </div>
     <div class="col-span-2">
       <cart-change />
