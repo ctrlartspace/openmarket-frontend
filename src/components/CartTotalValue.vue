8135a4d944803ade49e59e4b61df492c7890b0c2
Aman <beyond.http@gmail.com>
2023-11-17 16:57:00 +0600

    cart total icon

--- src/components/CartTotalValue.vue
+++ src/components/CartTotalValue.vue
@@ -2,7 +2,7 @@
   <div
     class="flex bg-green-400 text-white text-xl font-semibold p-4 border border-green-600 rounded hover:brightness-95 cursor-pointer font-mono shadow"
   >
-    <span class="material-icons self-center">shopping_cart</span>
+    <span class="material-icons self-center">check_circle</span>
     <span class="ml-4 flex-auto"> {{ store.getTotalAmount }} </span>
     <span class="">KZT</span>
   </div>
