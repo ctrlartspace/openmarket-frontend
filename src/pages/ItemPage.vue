3372839e178c4b205066fd6c762069f36e302c03
Aman <beyond.http@gmail.com>
2024-01-16 09:49:58 +0600

    item page redesign

--- src/pages/ItemPage.vue
+++ src/pages/ItemPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="grid grid-cols-10 bg-white rounded border border-gray-200">
-      <div v-if="item" class="col-span-4 border-r">
+      <div v-if="item" class="col-span-4 border-r border-b border-gray-200">
         <div class="px-4 py-2 border-b">
           <span class="text-lg font-semibold">Информация</span>
         </div>
@@ -38,26 +38,28 @@
           <filter-tree v-model="item.filters" :items="filterList" />
         </select-field>
       </div>
-      <div class="col-span-6 px-4 py-2 flex"></div>
-    </div>
-    <div
-      class="mt-4 inline-block bg-white border border-gray-200 rounded overflow-auto"
-    >
-      <div
-        v-if="!isEditMode"
-        class="px-4 py-2 text-blue-600 inline-flex gap-2 items-center border-l first:border-none cursor-pointer hover:bg-gray-50"
-        @click="isEditMode = true"
-      >
-        <span class="material-icons-outlined">edit</span>
-        <span class="text-lg">Редактировать</span>
-      </div>
-      <div
-        v-if="isEditMode"
-        class="px-4 py-2 text-blue-600 inline-flex gap-2 items-center border-l first:border-none cursor-pointer hover:bg-gray-50"
-        @click="saveItemData"
-      >
-        <span class="material-icons-outlined">save</span>
-        <span class="text-lg">Сохранить</span>
+      <div class="col-span-6 px-4 py-2 flex border-b border-gray-200"></div>
+      <div class="col-span-12 px-4 py-2 flex gap-4">
+        <button
+          v-if="!isEditMode"
+          class="text-blue-600 inline-flex gap-2 items-center hover:brightness-90"
+          @click="isEditMode = true"
+        >
+          <span class="text-lg">Редактировать</span>
+        </button>
+        <button
+          v-if="isEditMode"
+          class="text-blue-600 inline-flex gap-2 items-center hover:brightness-90"
+          @click="saveItemData"
+        >
+          <span class="text-lg">Сохранить</span>
+        </button>
+        <button
+          class="text-blue-600 inline-flex gap-2 items-center hover:brightness-90"
+          @click="saveItemData"
+        >
+          <span class="text-lg">Добавить</span>
+        </button>
       </div>
     </div>
   </div>
