44ffa452f7880e5be7ffc4bd42d7a280d8288b1f
Aman <beyond.http@gmail.com>
2024-01-18 10:04:26 +0600

    pwa setting

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,34 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  base: "./",
+  plugins: [
+    vue(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,jpg,png,svg,gif,webmanifest}"],
+      },
+      includeAssets: ["**/*.{png}"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "assets/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
