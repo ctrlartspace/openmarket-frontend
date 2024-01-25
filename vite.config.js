b99d4ba1b077f256d61447ba2f8f2de480feed14
Aman <beyond.http@gmail.com>
2024-01-25 11:11:16 +0600

    add https localhost

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
 import vue from "@vitejs/plugin-vue"
+import mkcert from "vite-plugin-mkcert"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: { https: true },
+  plugins: [vue(), mkcert()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
