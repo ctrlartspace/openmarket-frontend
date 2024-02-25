83f791e012e7c765dacf6c184058410ca63c81bb
Aman <beyond.http@gmail.com>
2024-02-25 16:40:04 +0600

    number input

--- src/pages/ScanPage.vue
+++ src/pages/ScanPage.vue
@@ -5,22 +5,48 @@
     <audio class="hidden" id="beepSound" controls preload="none">
       <source src="../assets/beep.wav" type="audio/wav" />
     </audio>
-    <div
-      v-if="!resultItem && !isStopped && !isNotFound"
-      class="p-4 text-center"
-    >
-      <p class="text-gray-300">Отсканируйте товар</p>
-    </div>
     <div v-if="isNotFound" class="p-4 text-red-600 text-center">
       <p>Товар не найден</p>
+    </div>
+    <div class="p-4 text-center">
       <button
-        class="px-4 mt-2 border border-red-600 rounded"
-        @click="resetScanner"
+        class="px-4 mt-2 border border-gray-600 rounded"
+        @click="isNumberInput = true"
       >
-        Повторить
+        Ввод цифр
       </button>
     </div>
     <div class="absolute bottom-12 left-0 right-0 p-4 mb-safe">
+      <div
+        v-if="isNumberInput"
+        class="relative p-4 flex flex-col gap-2 rounded-xl bg-white shadow-lg border border-gray-200"
+      >
+        <div class="p-4 text-2xl text-center border rounded-xl">
+          <p
+            class="tracking-widest"
+            :class="isLoading ? 'text-gray-300 animate-pulse' : ''"
+          >
+            {{ inputNumber }}
+          </p>
+        </div>
+        <div class="grid grid-cols-3 gap-2">
+          <button
+            v-for="i in 10"
+            class="px-2 py-2 border border-gray-200 rounded-xl text-xl text-center"
+            :class="i - 1 === 0 ? 'order-last col-span-2 ' : ''"
+            @click="setNumberInput(i - 1)"
+            :disabled="isLoading"
+          >
+            {{ i - 1 }}
+          </button>
+          <button
+            class="flex items-center justify-center px-2 py-2 bg-black text-white rounded-xl text-xl text-center order-last"
+            @click="isNumberInput = false"
+          >
+            <span class="material-icons-outlined">close</span>
+          </button>
+        </div>
+      </div>
       <div
         v-if="resultItem"
         class="relative p-4 rounded-xl bg-white shadow-lg border border-gray-200"
@@ -72,6 +98,20 @@ const isStopped = ref(false)
 const isNotFound = ref(false)
 const resultItem = ref(null)
 const itemCount = ref(1)
+const isNumberInput = ref(false)
+const inputNumber = ref("000000")
+const isLoading = ref(false)
+
+const setNumberInput = async (value) => {
+  if (inputNumber.value === "000000") {
+    inputNumber.value = ""
+  }
+
+  inputNumber.value += value
+  if (inputNumber.value.length >= 5) {
+    await checkItem(inputNumber.value)
+  }
+}
 
 const resetScanner = () => {
   resultItem.value = null
@@ -101,15 +141,20 @@ const addToCart = () => {
 
 const checkItem = async (id) => {
   try {
+    isLoading.value = true
     isNotFound.value = false
     const item = await getItem(id)
     resultItem.value = item
     isStopped.value = true
     const sound = document.getElementById("beepSound")
     sound.play()
+    isNumberInput.value = false
   } catch (error) {
     console.error(error)
     isNotFound.value = true
+  } finally {
+    inputNumber.value = "000000"
+    isLoading.value = false
   }
 }
 
